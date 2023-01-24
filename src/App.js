08954719c6bdc11afd4dcc73e29b12dfaa8b4a19
Liana <liannakharis@gmail.com>
2023-01-24 15:12:31 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,110 @@
-import logo from './logo.svg';
+import {Component} from 'react';
 import './App.css';
+
+// const Header = ()=> {
+//   return <h2>Hello world!</h2>
+// }
+
+// const Field = ()=> {
+//   const holder = 'Enter here';
+//   const styledField = {
+//     width: '300px'
+//   };
+//   return <input 
+//             placeholder={holder} 
+//             type="text" 
+//             style={styledField}/>
+// }
+
+// class Field extends Component {
+//   render() {
+//     const holder = 'Enter here';
+//   const styledField = {
+//     width: '300px'
+//   };
+//   return <input 
+//   placeholder={holder} 
+//   type="text" 
+//   style={styledField}/>
+//   }
+// }
+
+// function Btn() {
+//   const text = 'Log in';
+//   const logged = false;
+  
+//   return <button>{logged ? 'Enter' : text}</button>
+// }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <StrictMode> 
+//          <Header/>
+//          </StrictMode>
+//      <Field/>
+//      <Btn/>
+//     </div>
+//   );
+// }
+
+// export {Header};
+// export default App;
+
+
+class WhoAmI extends Component {
+  constructor(props){
+     super(props);
+     this.state = {
+          years: 27,
+          position: ''
+     }
+     
+    //  слева - обычное свойство которое будет у экземпляра, справа - жестко привязываем 
+    //  this.nextYear = this.nextYear.bind(this)
+  }
+
+  nextYear = () => {
+    console.log('+++');  
+    this.setState(state=> ({
+        years: state.years +1
+    }))
+  }
+
+  commitInputChanges = (e, color) => {
+    
+   this.setState({
+    position: e.target.value
+   })
+  }
+     render() {
+      const {name, surname, link} = this.props;
+      const {position, years} = this.state;
+       return (
+  <>
+   {/* передаем только название метода, аналог addEventListener */}
+    <button onClick={this.nextYear}>+++</button>
+    <h1>My name is {name}, 
+        surname - {surname}, 
+        age - {years}, 
+        position - {position}</h1>
+    <a href={link}>My profile</a>
+    <form>
+      <span>Введите должность</span>
+      <input type="text" onChange={(e)=> this.commitInputChanges(e, 'some color')}/>
+    </form>
+  </>
+)
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+     <WhoAmI name= 'John' surname="Smith" link="facebook.com"/>
+     <WhoAmI name= 'Alex' surname="Shepard" link="facebook.com"/>
     </div>
-  );
-}
+      )
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, {StrictMode} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Header } from './App';
+
+const text ="Hello World!"
+
+const elem = (
+  <StrictMode>
+    <App/>
+  </StrictMode>
+)
+
+;
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  elem,
 );
 
 // If you want to start measuring performance in your app, pass a function
